d6e4ae0fccaf61619613c604a2cfbbd49a345bdd
Дмитрий Носков <noskoff2@yandex.ru>
2022-05-29 15:08:27 +0300

    Initial commit

---
+++ src/main/java/ru/ifmo/mpi/magichospital/healer/HealersBackendApplication.java
@@ -0,0 +1,13 @@
+package ru.ifmo.mpi.magichospital.healer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealersBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealersBackendApplication.class, args);
+	}
+
+}
