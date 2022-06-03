57ef93706f9e080571e6a0f267033a86cb82da50
Дмитрий Носков <noskoff2@yandex.ru>
2022-06-03 17:07:34 +0300

    fixed cors

--- src/main/java/ru/ifmo/mpi/magichospital/healer/conf/SecurityConf.java
+++ src/main/java/ru/ifmo/mpi/magichospital/healer/conf/SecurityConf.java
@@ -1,5 +1,7 @@
 package ru.ifmo.mpi.magichospital.healer.conf;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +11,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import ru.ifmo.mpi.magichospital.healer.service.CustomUserDetailsService;
 
@@ -23,7 +28,8 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable()
 			.authorizeRequests().anyRequest().authenticated()
-			.and().httpBasic();
+			.and().httpBasic()
+			.and().cors();
     }
 	
     @Override
@@ -34,6 +40,16 @@ public class SecurityConf extends WebSecurityConfigurerAdapter {
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 	   return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		CorsConfiguration configuration = new CorsConfiguration();
+		configuration.applyPermitDefaultValues();
+		configuration.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE"));
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", configuration);
+		return source;
 	}
 
 	
